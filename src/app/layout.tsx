b89129d1b604d46cd55c89286ac8ed333ca211d9
the.rebel.jedi <82748115+PreyasGit@users.noreply.github.com>
2026-08-18 14:13:24 +0530

    build: install base shadcn ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -16,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
-      className={`${dmSans.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", dmSans.variable, "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col font-sans">{children}</body>
     </html>
